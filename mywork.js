1136caee9fce0e2c8a3e65a3e033b4cf10cadd03
ruthchukwumam <aruth71@gmail.com>
2015-07-07 13:14:33 +0100

    https://github.com/ruthchukwumam/readmeM.git

--- mywork.js
+++ mywork.js
@@ -15,3 +15,10 @@ do {
 }
 i++
 while i > 10
+	var students = ["Lovelyn", "Ibrahim", "Andrew"];
+Array.prototype.bootcamp = function (){
+for (i = 0; i < this.length; i++){
+	document.write(this[i] + " is in bootcamp." + "<br>");
+}
+}
+students.bootcamp();
