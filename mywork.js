a0a99ded2750df6dd7081c85c7b29a883ed738e0
ruthchukwumam <aruth71@gmail.com>
2015-07-07 17:50:24 +0100

    readmem

--- mywork.js
+++ mywork.js
@@ -22,3 +22,39 @@ for (i = 0; i < this.length; i++){
 }
 }
 students.bootcamp();
+
+function X (handsome, funny, mercedes){
+X.handsome = handsome;
+X.funny = funny;
+X.mercedes = mercedes;
+}
+
+function happy (X){
+    return (X.handsome && X.mercedes && X.funny);
+}
+function comfortable (X){
+    return (X.handsome && X.mercedes &&! X.funny);
+}
+function try (X){
+    return (!X.handsome && !X.mercedes && X,funny);
+}
+
+if happy (X){
+    console.log("I am happy");
+}
+if comfortable (X){
+    console.log ("I am comfortable");
+}
+if try (X){
+    console.log ("I will try");
+}
+
+x = 457;
+y = "457";
+x.length;
+y.length;
+x + parseInt(y);
+parseInt("45j")
+j= "(l)457(k)"
+var t= j.substring(3,6)
+parseInt(t)
